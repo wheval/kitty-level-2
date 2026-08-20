567792ba0511092359b66316ee5c6a22e0587e75
wheval <whevalmezeegbe@gmail.com>
2026-08-20 14:53:23 +0100

    Fix wallet module imports: use per-wallet subpath exports

--- frontend/src/lib/walletKit.ts
+++ frontend/src/lib/walletKit.ts
@@ -1,13 +1,10 @@
-import {
-  StellarWalletsKit,
-  FreighterModule,
-  xBullModule,
-  AlbedoModule,
-  LobstrModule,
-  RabetModule,
-  HanaModule,
-  Networks,
-} from '@creit.tech/stellar-wallets-kit'
+import { StellarWalletsKit, Networks } from '@creit.tech/stellar-wallets-kit'
+import { FreighterModule } from '@creit.tech/stellar-wallets-kit/modules/freighter'
+import { xBullModule } from '@creit.tech/stellar-wallets-kit/modules/xbull'
+import { AlbedoModule } from '@creit.tech/stellar-wallets-kit/modules/albedo'
+import { LobstrModule } from '@creit.tech/stellar-wallets-kit/modules/lobstr'
+import { RabetModule } from '@creit.tech/stellar-wallets-kit/modules/rabet'
+import { HanaModule } from '@creit.tech/stellar-wallets-kit/modules/hana'
 
 StellarWalletsKit.init({
   network: Networks.TESTNET,
